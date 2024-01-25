16139b709484ca77596119a94899b75b1462d3ef
toandz <103804127+toantretrou@users.noreply.github.com>
2024-01-25 10:45:33 +0700

    Add header responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'average': {'max': '850px'},
+        // => @media (max-width: 850px) { ... }
+
+        'small': {'max': '425px'},
+        // => @media (max-width: 425px) { ... }
+      },
+
       colors: {
         "cur-lang": "#ccc",
         gioi_thieu: "#8D8D8D",
       },
+      fontSize: {
+        'span': '1.15em'
+      },
+      fontWeight: {
+        'span': '600'
+      },
+      lineHeight: {
+        'span': '1.05'
+      },
+      letterSpacing: {
+        'span': '.05em'
+      }
     },
   },
   plugins: [],
