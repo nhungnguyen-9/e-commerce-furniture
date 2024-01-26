6d7793a3e0eb7f1cee0ee0e9f28257342817b907
nhungnguyen-9 <122953146+nhungnguyen-9@users.noreply.github.com>
2024-01-26 11:05:26 +0700

    Update CategoryList.jsx

--- src/app/components/CategoryList.jsx
+++ src/app/components/CategoryList.jsx
@@ -8,7 +8,7 @@ const CategoryList = (props) => {
         <div key={index} className="mb-3">
           <Link
             href={`#${item}`}
-            className="text-xl text-nowrap text-slate-500 hover:text-slate-950"
+            className="font-[14px] text-nowrap text-slate-500 hover:text-slate-950"
           >
             {item}
           </Link>
