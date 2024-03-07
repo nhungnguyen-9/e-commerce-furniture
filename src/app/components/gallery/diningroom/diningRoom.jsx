725f99b7bdf8994f3b20ea4659a2274a31c787ce
nhungnguyen-9 <122953146+nhungnguyen-9@users.noreply.github.com>
2024-03-07 11:53:02 +0700

    Delete src directory

--- src/app/components/gallery/diningroom/diningRoom.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DiningRoom() {
-    return (
-        <div>Dining Room Section</div>
-    )
-}
--- src/app/components/moreInfo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MoreInfo() {
-    return (
-        <div>moreInfo</div>
-    )
-}
