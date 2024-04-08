18f9ead2f9a9785186449d27f3e347ba236aded2
toandz <103804127+toantretrou@users.noreply.github.com>
2024-04-08 09:53:37 +0700

    Add search, sort product

--- src/app/components/products/ListProducts.jsx
+++ src/app/components/products/ListProducts.jsx
@@ -1,19 +1,58 @@
-'use client'
-import React from 'react'
-import FilterProducts from '@/app/components/products/FilterProducts'
-import ProductCard from './productCard'
+import React, { useState, useEffect } from 'react';
+import FilterProducts from '@/app/components/products/FilterProducts';
+import ProductCard from './productCard';
+import { usePathname, useSearchParams } from 'next/navigation'
+import { product } from '@/app/utils/product';
 
 export default function ListProducts({ data }) {
-    console.log(data)
+    const searchParams = useSearchParams()
+
+    const [filteredProducts, setFilteredProducts] = useState(data?.products);
+
+    useEffect(() => {
+        if (searchParams && searchParams.has('search'))
+        {
+            const searchValue = searchParams.get('search');
+            searchProducts(searchValue);
+        } else {
+            // Nếu không có tham số tìm kiếm, hiển thị tất cả sản phẩm
+            setFilteredProducts(data?.products || []);
+          }
+      }, [data, searchParams])
+
+    const searchProducts = (search) => {
+        if (data && data.products && data.products.length > 0) {
+            const filtered = data.products.filter(product => 
+                product.name.toLowerCase().includes(search.toLowerCase())
+            );
+            
+            setFilteredProducts(filtered);
+        }
+    };
+
+    const handleFilterChange = (filterType) => {
+        let filtered = filteredProducts.slice();
+
+        if (filterType.value === 'low_to_high') {
+            filtered.sort((a, b) => a.price - b.price);
+        } else if (filterType.value === 'high_to_low') {
+            filtered.sort((a, b) => b.price - a.price);
+        } else if (filterType.value === 'newest') {
+            filtered.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+        }
+
+        setFilteredProducts(filtered);
+    };
+
     return (
         <div className='max-w-[1320px] mx-auto'>
-            <FilterProducts />
+            <FilterProducts onFilterChange={handleFilterChange} />
 
             <div className='grid grid-cols-4'>
-                {data?.products?.map((product) => {
-                    return <ProductCard key={product?.slug} product={product} />
+                {filteredProducts?.map((product) => {
+                    return <ProductCard key={product?.slug} product={product} />;
                 })}
             </div>
         </div>
-    )
+    );
 }
